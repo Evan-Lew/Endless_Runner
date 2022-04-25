a8f1f18bcfb0d835f366c72614be6819b0032388
lxu58 <103284069+lxu58@users.noreply.github.com>
2022-04-25 06:34:35 -0700

    major update soloscene
    4/25
    Update solo version of game
    Add feature "Novice mode, which starts from 0 second to 10 second, generate 1 asteroids from 8 random direction"
    Add feature "Expert mode will be turned on after 10 seconds, generate 2 asteroids at same time."

--- example/src/prefabs/Slime.js
+++
@@ -1,9 +0,0 @@
-class Slime extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
-        super(scene, x, y, texture, frame);
-
-        // add object to existing scene
-        scene.add.existing(this);
-
-    }
-}
